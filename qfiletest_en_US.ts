2c0fb8af15d46a199ceda61efc25176b6ba32ac7
c0de4un <code4un@yandex.ru>
2020-01-31 09:44:42 +0400

    (fix) switch to qml

--- qfiletest_en_US.ts
+++ qfiletest_en_US.ts
@@ -1,13 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="qfiletest_en_US">
+<TS version="2.1" language="en_US">
 <context>
-    <name>MainWindow</name>
+    <name>main</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <location filename="build-qfiletest-Desktop_Qt_5_12_6_MinGW_32_bit-Debug/ui_mainwindow.h" line="50"/>
-        <location filename="build-qfiletest-Desktop_Qt_5_12_6_MinGW_32_bit-Release/ui_mainwindow.h" line="50"/>
-        <source>MainWindow</source>
+        <location filename="src/qml/main.qml" line="8"/>
+        <source>Hello World</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
